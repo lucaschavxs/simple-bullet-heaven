450f3cb458cc665bc109854b7ee2945fe8625aac
Lukiyo <lucasschavescosta@gmail.com>
2026-05-13 18:38:46 -0300

    feat: implement basic player movement
    Implemented input handling and physics movement for the player character using move_and_slide. Also renamed the root node to 'Player' and cleaned up the scene transform.

--- player.gd
+++ player.gd
@@ -2,3 +2,12 @@ extends CharacterBody2D
 
 #teste novo teste novo
 #git test changes
+func _physics_process(_delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * 600
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		get_node("HappyBoo").play_walk_animation()
+	else:
+		get_node("HappyBoo").play_idle_animation()
